4215338c63f729dc176c779854a9e2aedfe344fa
matejmarek <131402700+matejmarek@users.noreply.github.com>
2023-08-25 22:07:22 +0200

    Update newList.js

--- version_12/js/newList.js
+++ version_12/js/newList.js
@@ -1,1 +1,1 @@
-export const videoNameList = ["marathon6", "matrix", "monkey", "monkey-9", "monkey_3", "train-7", "transparent_fish", "uav1", "umbrella-19", "wheel", "whitefish", "wubble_bubble", "yoyo-7", "zebra-16", "zebrafish1"]
+export const videoNameList = ["marathon6", "matrix", "monkey", "monkey-9", "monkey_3", "train-7", "transparent_fish", "uav1", "umbrella-19", "wheel", "whitefish", "zebra-16", "zebrafish1"]
